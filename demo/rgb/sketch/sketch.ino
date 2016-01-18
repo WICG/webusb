0664e9b3014203b4bd0454c7a3ba69ae3144424d
Martino Facchin <m.facchin@arduino.cc>
2016-01-18 14:15:12 +0100

    update sketch to use WebUSB library

--- demo/rgb/sketch/sketch.ino
+++ demo/rgb/sketch/sketch.ino
@@ -1,3 +1,7 @@
+#include <WebUSB.h>
+
+#define Serial WebSerial
+
 const int redPin = 9;
 const int greenPin = 10;
 const int bluePin = 11;
@@ -8,8 +12,8 @@ void setup() {
   while (!Serial) {
     ;
   }
-  Serial.write("Sketch begins.\r\n");
   Serial.begin(9600);
+  Serial.write("Sketch begins.\r\n");
   index = 0;
 }
 
